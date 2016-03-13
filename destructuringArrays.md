28527979dc10a9349607fed21a27db79302a22ec
Raghava Ankem <Raghava Ankem>
2016-03-13 18:44:25 -0500

    starting to learn destructuring

--- destructuringArrays.md
+++ destructuringArrays.md
@@ -1,1 +1,10 @@
 #Destructuring Arrays
+
+In locations that receive the data we can use patern matching to destructure values.
+This can be left side of assaginment opetator or arguments to functions etc.  
+
+**Object Destructuring**
+
+`let obj = {first: 'firstName', last: 'lastName'};
+ let {first: f, last: l} = obj;
+ // we will have two variables declared and initialized f with firstName and l with secondName` 
---
+++ test/destructring.spec.js
@@ -0,0 +1,25 @@
+import {expect} from 'chai';
+import {Chance} from 'chance';
+
+describe('destructuring', () => {
+    let chance,
+        expectedFirstName,
+        expectedLastName;
+
+    before(() => {
+        chance = new Chance(),
+            expectedFirstName = chance.string(),
+            expectedLastName = chance.string();
+    });
+
+    describe('Object Destructuring', () => {
+        it('should destructure from object', () => {
+            let obj = { firstName: expectedFirstName, lastName: expectedLastName };
+
+            let {firstName: f, lastName: l} = obj;
+
+            expect(f).to.be.equal(expectedFirstName);
+            expect(l).to.be.equal(expectedLastName);
+        });
+    });
+});
